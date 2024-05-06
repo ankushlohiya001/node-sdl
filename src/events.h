cb440620814633e088780cd7cdfc05da60dc5b24
ankushlohiya001 <ankushlohiya2000@gmail.com>
2024-05-06 23:09:42 +0530

    just formatted code by editor :D

--- src/events.h
+++ src/events.h
@@ -1,29 +1,30 @@
 #ifndef _NodeSdl_Events_h
 #define _NodeSdl_Events_h
 
-#include <napi.h>
 #include <SDL.h>
+#include <napi.h>
 
-namespace Events{
-	
-	class EventWatcher : public Napi::ObjectWrap<EventWatcher>{
-	public:
-		EventWatcher(const Napi::CallbackInfo&);
-		void pollEvent(const Napi::CallbackInfo&);
-		void setCallback(const Napi::CallbackInfo&);
+namespace Events {
 
-		void setCommonEvent(Napi::Env&, Napi::Object&);
-		Napi::Value getWindowEvent(const Napi::CallbackInfo&);
-		Napi::Value getMouseEvent(const Napi::CallbackInfo&);
-		Napi::Value getKeyboardEvent(const Napi::CallbackInfo&);
+class EventWatcher : public Napi::ObjectWrap<EventWatcher> {
+public:
+  EventWatcher(const Napi::CallbackInfo &);
+  void pollEvent(const Napi::CallbackInfo &);
+  void setCallback(const Napi::CallbackInfo &);
 
-		static Napi::Function getClass(Napi::Env&);
-	private:
-		SDL_Event eve;
-		Napi::FunctionReference cbRef;
-	};
-	
-	void Init(Napi::Env&, Napi::Object&);
-}
+  void setCommonEvent(Napi::Env &, Napi::Object &);
+  Napi::Value getWindowEvent(const Napi::CallbackInfo &);
+  Napi::Value getMouseEvent(const Napi::CallbackInfo &);
+  Napi::Value getKeyboardEvent(const Napi::CallbackInfo &);
+
+  static Napi::Function getClass(Napi::Env &);
+
+private:
+  SDL_Event eve;
+  Napi::FunctionReference cbRef;
+};
+
+void Init(Napi::Env &, Napi::Object &);
+} // namespace Events
 
 #endif
--- src/sdl.h
+++ src/sdl.h
@@ -1,16 +1,16 @@
 #ifndef _NodeSdl_Sdl_H_
 #define _NodeSdl_Sdl_H_
 
-#include <napi.h>
 #include <SDL.h>
+#include <napi.h>
 
-namespace SdlMain{
+namespace SdlMain {
 
-  Napi::Value initSome(const Napi::CallbackInfo& info);
-  Napi::Value initSubSystem(const Napi::CallbackInfo& info);
-  void quitSubSystem(const Napi::CallbackInfo& info);
-  Napi::Value wasInit(const Napi::CallbackInfo& info);
-  void quit(const Napi::CallbackInfo& info);
-}
+Napi::Value initSome(const Napi::CallbackInfo &info);
+Napi::Value initSubSystem(const Napi::CallbackInfo &info);
+void quitSubSystem(const Napi::CallbackInfo &info);
+Napi::Value wasInit(const Napi::CallbackInfo &info);
+void quit(const Napi::CallbackInfo &info);
+} // namespace SdlMain
 
 #endif // _NodeSdl_Sdl_H_
